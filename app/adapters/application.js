639cbd1eb6832a7133fc93beeee3839d385e48dd
Dylan <dpruitt.developer@gmail.com>
2022-05-28 19:04:47 -0400

    Use rails server for backend

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,5 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
-  namespace = 'api';
-
-  buildURL(...args) {
-    return `${super.buildURL(...args)}.json`;
-  }
+  host = 'http://localhost:3000';
 }
