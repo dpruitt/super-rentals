9e3bee9289809df5f69624957d537fdb7aebb321
Dylan <dpruitt.developer@gmail.com>
2022-05-30 13:52:24 -0400

    Flatten location attribute

--- app/controllers/add-rental.js
+++ app/controllers/add-rental.js
@@ -4,15 +4,35 @@ import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class AddRentalController extends Controller {
+  @service store;
   @service router;
   @service flashMessages;
 
-  @tracked rental;
+  @tracked title;
+  @tracked owner;
+  @tracked city;
+  @tracked lat;
+  @tracked lng;
+  @tracked category;
+  @tracked image;
+  @tracked bedrooms;
+  @tracked description;
 
   @action
   async saveRental() {
     try {
-      await this.rental.save();
+      let rental = this.store.createRecord('rental', {
+        title: this.title,
+        owner: this.owner,
+        city: this.city,
+        lat: this.lat,
+        lng: this.lng,
+        category: this.category,
+        image: this.image,
+        bedrooms: this.bedrooms,
+        description: this.description
+      });
+      await rental.save();
       this.router.transitionTo('/');
     } catch(error) {
       console.log(error);
