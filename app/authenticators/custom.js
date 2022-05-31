5d490f5bc488af7092b39bfe4dfa513ffd59bc91
Dylan <dpruitt.developer@gmail.com>
2022-05-31 17:03:28 -0400

    Stay authenticated on refresh

--- app/authenticators/custom.js
+++ app/authenticators/custom.js
@@ -3,9 +3,17 @@ import Base from 'ember-simple-auth/authenticators/base';
 export default class CustomAuthenticator extends Base {
   authenticate(username, password) {
     if(username === "test" && password === "test") {
-      return Promise.resolve();
+      return Promise.resolve({authenticated: true});
     } else {
       return Promise.reject(new Error("Authentication failed. There is only one true user."));
+    }
+  }
+
+  restore(data) {
+    if(data.authenticated === true) {
+      return Promise.resolve({authenticated: true});
+    } else {
+      return Promise.reject();
     }
   }
 }
